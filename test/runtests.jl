571da247d8a3ee3201eca0faceeffce99e2b1956
Zhiping Zhang <shouwangzpz@icloud.com>
2019-10-07 18:33:37 -0400

    Add files via upload

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,5 @@ using GFFJ, Test
 # using JuMP, Gurobi, CPLEX, GLPK
 
 println("Starting tests....................")
-@time begin include("testGapFill.jl") end
 @time begin include("testGapFind.jl") end
+@time begin include("testGapFill.jl") end
